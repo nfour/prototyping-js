60d806c9aa07a6d9325ec0466f49d0110cfd46a1
nfour <novus.nfour@gmail.com>
2018-09-12 16:46:38 +1000

    wip

---
+++ react/src/mobxStateTree/MobXTreeTabs.tsx
@@ -0,0 +1,46 @@
+
+import { Tab, Tabs as MuiTabs } from '@material-ui/core';
+import { observer } from 'mobx-react';
+import * as React from 'react';
+
+export interface IProps {
+  ButtonContainer?: React.ComponentType;
+  ContentContainer?: React.ComponentType;
+  tabs: Array<{ label: string, Content: React.ComponentType }>;
+  tabState: ITabState;
+}
+
+export interface ITabState {
+  active: number;
+  setActiveTab (index: number): any;
+}
+
+const DefaultButtonContainer: React.SFC = ({ children }) => <div>{children}</div>;
+const DefaultContentContainer: React.SFC = ({ children }) => <div>{children}</div>;
+
+export const MobXTreeTabs: React.SFC<IProps> = ({
+  ButtonContainer = DefaultButtonContainer,
+  ContentContainer = DefaultContentContainer,
+  tabState: { active, setActiveTab },
+  tabs,
+}) => {
+  const { Content } = tabs[active];
+
+  return (
+    <div>
+      <ButtonContainer>
+        <MuiTabs value={active} onChange={(event, value) => setActiveTab(value)}>
+          {tabs.map(({ label }, index) =>
+            <Tab value={index} label={label} key={index} />,
+          )}
+        </MuiTabs>
+      </ButtonContainer>
+      <ContentContainer>
+        <Content />
+      </ContentContainer>
+    </div>
+  );
+};
+
+/** The `observer` decorator means that whenever it detects a store change, this component shall re-render */
+export const ControlledMobXTreeTabs = observer(MobXTreeTabs);
---
+++ react/src/mobxStateTree/stories.tsx
@@ -0,0 +1,28 @@
+
+import { storiesOf } from '@storybook/react';
+import { observable } from 'mobx';
+import * as React from 'react';
+import { ControlledMobXTreeTabs } from './MobXTreeTabs';
+
+const tabState = observable({
+  active: 0,
+  setActiveTab (active: number) {
+    tabState.active = active;
+  },
+});
+
+const stories = storiesOf('MobX State', module);
+
+/**
+ * This component is controlled by the MobX store, `tabState`.
+ * Whenever `setActiveTab` is called, this component will re-render reactively
+ */
+stories.add('ControlledMobXTabs', () =>
+  <ControlledMobXTreeTabs
+    tabs={[
+      { label: 'Foo', Content: () => <span>Foo</span> },
+      { label: 'Bar', Content: () => <span>Bar</span> },
+    ]}
+    tabState={tabState}
+  />,
+);
